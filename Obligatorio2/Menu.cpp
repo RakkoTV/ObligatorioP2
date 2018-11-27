2031269dc7a5f48ccd124b9e93359a662c0d548a
Ramiro <ramiro.silva.1993@gmail.com>
2018-11-26 22:11:54 -0300

    Se sube Menu

---
+++ Obligatorio2/Menu.cpp
@@ -0,0 +1,24 @@
+#include "Menu.h"
+
+
+void MostrarMenu(int opcion)
+{
+    switch(opcion)
+    {
+
+        case 1:
+        {
+
+        }
+
+            break;
+
+        case 2:
+
+            break;
+
+
+    }
+
+
+}
